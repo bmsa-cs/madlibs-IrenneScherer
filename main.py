1fecfced79cfd3f55aebcc86bc4e63f5bae10b8b
Irenne Scherer <ischerer24@biomedscienceacademy.org>
2020-11-09 16:39:02 +0000

    I made changed it to have the f command and I fixed some of the bugs in my code

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
-
+Author:Irenne Scherer
+Period/Core:2
 """
+
+
+print ("Let's Play Silly Sentences")
+
+name1 = input ("\nEnter a name: ")
+
+number1 = input ("Enter a number between 1-24: ")
+
+plural_noun1 = input ("Enter a plural noun: ")
+
+location1 = input ("Enter a location: ")
+
+name2 = input ("Enter another name: ")
+
+large_number = input ("Enter a large number: ")
+
+adjective1 = input ("Enter an adjective: ")
+
+adjective2 = input ("Enter another adjective: ")
+
+pt_verb1 = input ("Enter a past tense verb: ")
+
+year1 = input ("Enter a year: ")
+
+food1 = input ("Enter a food: ")
+
+food2 = input ("Enter another food: ")
+
+food3 = input ("Enter another food: ")
+
+adverb1 = input ("Enter an adverb: ")
+
+noun1 = input ("Enter a noun: ")
+
+adverb2 = input ("Enter another adverb: ")
+
+month = input ("What month is it? ")
+
+day = input ("What day of the monthe is it?(numerical form) ").upper
+
+
+
+
+print ("\n\n Hello my fellow brainiacs, this is "+name1+" its "+number1+" o-clock, and I'm back with your daily dose of fun facts. Today's topic is dinosaurs!")
+print (f"Did you know that archeologists recently found a new set of {plural_noun1} in {location1} for the Brachiosaurus. \nThey also discovered a new dinosaur called {name2}iasaurous dinosaur with {large_number} bones. This new {name2}iasaurous is very {adjective1} and {adjective2} and is presumed to have {pt_verb1} around the world in {year1} BCE. Its diet consisted of {food1}, {food2}, and {food3}. It seemed to really enjoy {adverb1} on the {noun1} during the daytime, and {adverb2} during the nighttime.")
+print("\nThank you for tuning in, I've been "+name1+", have a good rest of "+month+" "+day+"th(st,nd,rd).")
+
